6fb6360754de642a768a79a57198e1599354d51f
Zev Goldstein <zev.goldstein@gmail.com>
2018-08-31 13:36:06 -0400

    initial commit

---
+++ deptest.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	bystander "github.com/zevdg/deptest-bystander"
+	sauce "github.com/zevdg/deptest-secret-sauce"
+)
+
+func main() {
+	bystander.StatusReport()
+	sauce.Dip()
+}
